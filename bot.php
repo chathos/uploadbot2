2c88666c4860eee62e7e1f95cc61e375ff41334f
Shrimadhav U K <shrimadhavuk@gmail.com>
2018-05-11 23:22:10 +0530

    A Major ReWrite

--- bot.php
+++ bot.php
@@ -1,21 +1,24 @@
 #!/usr/bin/env php
 <?php
 
-require 'vendor/autoload.php';
+require_once __DIR__ . "/config.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use danog\MadelineProto\API;
 use danog\MadelineProto\Exception;
 use danog\MadelineProto\Logger;
-use Kunnu\Dropbox\Dropbox;
-use Kunnu\Dropbox\DropboxApp;
-use Kunnu\Dropbox\DropboxFile;
 
-const BOT_SESSION = 'teleupload.madeline';
-const TMP_DOWNLOADS = '.' . DIRECTORY_SEPARATOR . 'tmp_downloads';
-$dropbox = new Dropbox(new DropboxApp(getenv("DB_ID"), getenv("DB_SECRET"), getenv("DB_TOKEN")));
+const BOT_SESSION = "YAUploadBot.session";
+const TMP_DOWNLOADS = __DIR__ . '/temporary_downloads';
+
 set_include_path(get_include_path() . ':' . realpath(dirname(__FILE__) . '/MadelineProto/'));
 
-$settings = ['app_info' => ['api_id' => 246968, 'api_hash' => 'dd9b27c65c119f3b82ac036859e77b53']];
+$settings = [
+  'app_info' => [
+    'api_id' => $APP_ID,
+    'api_hash' => $API_HASH
+  ]
+];
 
 try {
     $MadelineProto = new API(BOT_SESSION, $settings);
@@ -23,10 +26,12 @@ try {
     $MadelineProto = new API($settings);
 }
 
-$authorization = $MadelineProto->bot_login(getenv("BOT_TOKEN"));
+$authorization = $MadelineProto->bot_login($BOT_TOKEN);
 
-if (!file_exists(TMP_DOWNLOADS))
-    mkdir(TMP_DOWNLOADS);
+if (!file_exists(TMP_DOWNLOADS)){
+  mkdir(TMP_DOWNLOADS);
+}
+
 $MadelineProto->session = BOT_SESSION;
 $offset = 0;
 $conversations = array();
@@ -45,38 +50,82 @@ while (true) {
                     $destination = retrieveDestination($update);
                     if (isset($update['update']['message']['media']) && (retrieveFromMessage($update, 'media')['_'] == 'messageMediaPhoto' || retrieveFromMessage($update, 'media')['_'] == 'messageMediaDocument')) {
                         $time = time();
-                        $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Downloading file...', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                        $file = $MadelineProto->download_to_file($update['update']['message']['media'], TMP_DOWNLOADS . DIRECTORY_SEPARATOR . $update['update']['message']['media']['document']['attributes'][0]['file_name']);
-                        $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Downloaded in ' . (time() - $time) . ' seconds', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                        $conversations[$destination] = array('downloadDir' => $file, 'fileName' => getFileName($file, DIRECTORY_SEPARATOR));
+                        $MadelineProto->messages->sendMessage([
+                          'peer' => $destination,
+                          'message' => 'Downloading file...',
+                          'reply_to_msg_id' => retrieveFromMessage($update, 'id')
+                        ]);
+                        $file = $MadelineProto->download_to_file(
+                          $update['update']['message']['media'],
+                          TMP_DOWNLOADS . DIRECTORY_SEPARATOR . $update['update']['message']['media']['document']['attributes'][0]['file_name']
+                        );
+                        $MadelineProto->messages->sendMessage([
+                          'peer' => $destination,
+                          'message' => 'Downloaded in ' . (time() - $time) . ' seconds',
+                          'reply_to_msg_id' => retrieveFromMessage($update, 'id')
+                        ]);
+                        $conversations[$destination] = array(
+                          'downloadDir' => $file,
+                          'fileName' => getFileName($file, DIRECTORY_SEPARATOR)
+                        );
                     } else if (isset($update['update']['message']['message'])) {
                         $message = retrieveFromMessage($update, 'message');
                         if (startsWith($message, 'http://') || startsWith($message, 'https://') || startsWith($message, 'ftp://')) {
-                            $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Downloading file...', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
+                            $MadelineProto->messages->sendMessage([
+                              'peer' => $destination,
+                              'message' => 'Downloading file...',
+                              'reply_to_msg_id' => retrieveFromMessage($update, 'id')
+                            ]);
+                            // TODO: Progress CallBack for Download Function 
                             try {
                                 $conversations[$destination] = downloadFile($message);
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'File downloaded!', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
+                                $MadelineProto->messages->sendMessage([
+                                  'peer' => $destination,
+                                  'message' => 'File downloaded!',
+                                  'reply_to_msg_id' => retrieveFromMessage($update, 'id')
+                                ]);
+                                // successfully downloaded, now start the upload
+                                $sentMessage = $MadelineProto->messages->sendMedia([
+                                  'peer' => $destination,
+                                  'media' => [
+                                    '_' => 'inputMediaUploadedDocument',
+                                    'file' => new \danog\MadelineProto\FileCallback(
+                                      $conversations[$destination]['downloadDir'],
+                                      function ($progress) use ($MadelineProto, $destination) {
+                                        $MadelineProto->messages->sendMessage([
+                                          'peer' => $destination,
+                                          'message' => 'Upload progress: '.$progress.'%'
+                                        ]);
+                                      }
+                                    ),
+                                    'attributes' => [
+                                      [
+                                        '_' => 'documentAttributeVideo',
+                                        'round_message' => false,
+                                        'supports_streaming' => true
+                                      ]
+                                    ]
+                                  ],
+                                  'message' => 'Uploaded using @MadeLineProto ',
+                                  'parse_mode' => 'Markdown'
+                                ]);
+                                // var_dump($sentMessage);
+                                // TODO: delete the original file, after successful UPLOAD
                             } catch (Exception $e) {
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Unable to download file', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
+                                // var_dump($e);
+                                $MadelineProto->messages->sendMessage([
+                                  'peer' => $destination,
+                                  'message' => 'Unable to download file',
+                                  'reply_to_msg_id' => retrieveFromMessage($update, 'id')
+                                ]);
                             }
-                        } else if ($message == '/dropbox') {
-                            if (isset($conversations[$destination])) {
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Uploading file...', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                                $dropbox->upload(new DropboxFile($conversations[$destination]['downloadDir']), DIRECTORY_SEPARATOR . $conversations[$destination]['fileName'], ['autorename' => true]);
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Uploaded!', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                            } else
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'You need to send a file first', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                        } else if ($message == '/telegram') {
-                            if (isset($conversations[$destination])) {
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'Uploading file...', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                                $file = ['_' => 'inputMediaUploadedDocument', 'file' => $MadelineProto->upload($conversations[$destination]['downloadDir']), 'mime_type' => 'magic/magic', 'caption' => '', 'attributes' => [['_' => 'documentAttributeFilename', 'file_name' => $conversations[$destination]['fileName']]]];
-                                $MadelineProto->messages->sendMedia(['peer' => $destination, 'media' => $file, 'message' => 'Uploaded via tele_uploader', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
-                            } else
-                                $MadelineProto->messages->sendMessage(['peer' => $destination, 'message' => 'You need to send a file first', 'reply_to_msg_id' => retrieveFromMessage($update, 'id')]);
                         }
                     }
                 } catch (RPCErrorException $e) {
-                    $MadelineProto->messages->sendMessage(['peer' => '@ilteoood', 'message' => $e->getCode() . ': ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString()]);
+                    $MadelineProto->messages->sendMessage([
+                      'peer' => '@SpEcHiDe',
+                      'message' => $e->getCode() . ': ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString()
+                    ]);
                 }
         }
     }
@@ -88,12 +137,49 @@ function getFileName($filePath, $separator)
     return $splitted[count($splitted) - 1];
 }
 
+/*function progressCallback( $download_size, $downloaded_size, $upload_size, $uploaded_size )
+{
+    static $previousProgress = 0;
+
+    if ( $download_size == 0 )
+        $progress = 0;
+    else
+        $progress = round( $downloaded_size * 100 / $download_size );
+
+    if ( $progress > $previousProgress)
+    {
+        $previousProgress = $progress;
+        global $MadelineProto;
+        $MadelineProto->messages->sendMessage([
+          'peer' => $to,
+          'message' => $message,
+          'reply_to_msg_id' => $replyTo
+        ]);
+    }
+}
+
+// https://gist.github.com/bdunogier/1030450
+
+function downloadRemoteFile($url, $destination_file)
+{
+  $targetFile = fopen( $destination_file, 'w' );
+  $ch = curl_init( $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt( $ch, CURLOPT_NOPROGRESS, false );
+  curl_setopt( $ch, CURLOPT_PROGRESSFUNCTION, 'progressCallback' );
+  curl_setopt( $ch, CURLOPT_FILE, $targetFile );
+  curl_exec( $ch );
+  fclose( $targetFile );
+}*/
+
 function downloadFile($message)
 {
-    $fileName = getFileName($message, '/');
+  var_dump($message);
+    $fileName = getFileName($message, "/");
     $downloadDir = TMP_DOWNLOADS . DIRECTORY_SEPARATOR . $fileName;
-    if (!file_exists($downloadDir))
-        file_put_contents($downloadDir, fopen("$message", 'r'));
+    if (!file_exists($downloadDir)){
+      file_put_contents($downloadDir, fopen($message, 'r'));
+    }
     return array('downloadDir' => $downloadDir, 'fileName' => $fileName);
 }
 
---
+++ config.sample.php
@@ -0,0 +1,6 @@
+<?php
+  // Login to my.telegram.org
+  $APP_ID = "YOUR_APP_ID_HERE";
+  $API_HASH = "YOUR_API_HASH_HERE";
+  // Get this from @BotFather
+  $BOT_TOKEN = "YOUR_TOKEN_HERE";
